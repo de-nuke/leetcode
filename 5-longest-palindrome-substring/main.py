4c728f09b8cdd2dac8983914b0d4d8c521a1fa90
michalsut <sut.michal@gmail.com>
2023-09-18 20:51:32 +0200

    Format code with black

--- 5-longest-palindrome-substring/main.py
+++ 5-longest-palindrome-substring/main.py
@@ -2,58 +2,70 @@ TESTS = {
     "babad": ["bab", "aba"],
     "cbbd": ["bb"],
     "aaaaa": ["aaaaa"],
-    "abcdefgh": ["a"],
+    "abcdefgh": ["a", "b", "c", "d", "e", "f", "g", "h"],
     "": [""],
     "abaffkajak": ["kajak"],
     "abbcccbbbcaaccbababcbcabca": ["bbcccbb"],
-    "cbcdcbedcbc": ["bcdcb"]
+    "cbcdcbedcbc": ["bcdcb"],
 }
 
+
 class Solution:
+    """
+    Longest palindrome substring
+    https://leetcode.com/problems/longest-palindromic-substring
+
+    The solution is an implementation of Manacher's algorithm.
+    """
+
     def longestPalindrome(self, s: str) -> str:
         s = "#" + "#".join(s) + "#"
 
         str_len = len(s)
-        l, r = 0, 0  # boundaries (left, right) of current longest palindrome. Initially, it is empty string
-        radiuses = {}  # Mapping index to a radius of longest palindrome, which has center at this index.
+        left, right = 0, 0  # boundaries of current longest palindrome
+        radiuses = {}  # Mapping palindrome center to its radius
 
         for center in range(0, str_len):
             radius = 0
 
             # Manacher's algorithm – this is a part that speeds up computation
-            if center < r:
-                mirrored_center = l + (r - center)
+            if center < right:
+                mirrored_center = left + (right - center)
                 mirrored_radius = radiuses[mirrored_center]
 
-                if center + mirrored_radius < r:
+                if center + mirrored_radius < right:
                     radiuses[center] = mirrored_center
                     continue
 
-                # Can't take mirrored radius directly, because we may skip some chars
-                # between r and center + mirrored radius
-                radius = min(r-center, mirrored_radius)
+                # Can't take mirrored radius directly, because we may skip some
+                # chars between right and center + mirrored radius
+                radius = min(right - center, mirrored_radius)
 
             # Trivial Algorithm:
             while (
-                    center - radius >= 0 and
-                    center + radius < str_len and
-                    s[center - radius] == s[center + radius]
+                center - radius >= 0
+                and center + radius < str_len
+                and s[center - radius] == s[center + radius]
             ):
                 radius += 1
-            radius = radius - 1  # Because when loop stopped, the radius was already too big by one
 
+            # Loop stops after radius is incremented,
+            # so it must be decremented to get the last "valid" value
+            radius = radius - 1
             radiuses[center] = radius
 
             # Update longest palindrome
-            if (2 * radius + 1) > (r + 1 - l):
-                l = center - radius
-                r = center + radius
-        return s[l:r+1].replace("#", "")
+            if (2 * radius + 1) > (right + 1 - left):
+                left = center - radius
+                right = center + radius
+        return s[left : right + 1].replace("#", "")
 
 
 if __name__ == "__main__":
     solution = Solution()
-    method = getattr(solution, list(filter(lambda m: not m.startswith("_"), dir(solution)))[0])
+    method = getattr(
+        solution, list(filter(lambda m: not m.startswith("_"), dir(solution)))[0]
+    )
     for _input, expected_output in TESTS.items():
         actual_output = method(_input)
         if actual_output in expected_output:
